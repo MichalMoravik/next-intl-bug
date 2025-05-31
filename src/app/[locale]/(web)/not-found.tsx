0001038a9c4c65e0a3f4a5c74bbdc20144d0802a
MichalMoravik <michal.moravik@icloud.com>
2025-05-31 22:21:28 +0200

    added one more not-found page to be more clear that it's not about this page actually

---
+++ src/app/[locale]/(web)/not-found.tsx
@@ -0,0 +1,5 @@
+// this page is irrelevant for debugging the bug.
+// the bug persists even if this page is not present.
+export default function NotFound() {
+  return <div>Not found in [locale]</div>;
+}
